d99dc688a0de312630108ecda7acaad872c3ba34
Lechuck Roh <lechuckroh@gmail.com>
2023-05-21 00:53:38 +0900

    add release configuration for umbrella project

--- hello_umbrella/mix.exs
+++ hello_umbrella/mix.exs
@@ -7,7 +7,15 @@ defmodule Hello.Umbrella.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      releases: [
+        hello_umbrella: [
+          applications: [
+            hello: :permanent,
+            hello_web: :permanent
+          ]
+        ]
+      ]
     ]
   end
 
