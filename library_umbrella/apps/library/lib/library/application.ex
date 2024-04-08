6e8397d2a88a68caecbd88257cbd85eb7f5259be
LechuckRoh <lechuckroh@gmail.com>
2024-04-08 22:37:49 +0900

    generate library project

---
+++ library_umbrella/apps/library/lib/library/application.ex
@@ -0,0 +1,23 @@
+defmodule Library.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      Library.Repo,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Library.PubSub},
+      # Start Finch
+      {Finch, name: Library.Finch}
+      # Start a worker by calling: Library.Worker.start_link(arg)
+      # {Library.Worker, arg}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one, name: Library.Supervisor)
+  end
+end
