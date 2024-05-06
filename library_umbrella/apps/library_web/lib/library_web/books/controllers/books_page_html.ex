bd21fb3b773dc9faa209e68ea1b9534bd88a77ae
LechuckRoh <lechuckroh@gmail.com>
2024-05-06 21:42:47 +0900

    use daisyui for book cover

--- library_umbrella/apps/library_web/lib/library_web/books/controllers/books_page_html.ex
+++ library_umbrella/apps/library_web/lib/library_web/books/controllers/books_page_html.ex
@@ -7,15 +7,19 @@ defmodule LibraryWeb.BooksPageHTML do
 
   def book_cover(assigns) do
     ~H"""
-    <h2><%= @book.title %></h2>
-    <ul>
-      <li>ISBN: <%= @book.isbn %></li>
-      <li>Edition: <%= @book.edition %></li>
-      <li>Publisher: <%= @book.publisher %></li>
-      <li>Publication Date: <%= @book.publication_date %></li>
-    </ul>
-    <div class="btn btn-success">
-    Download
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title"><%= @book.title %></h2>
+        <ul>
+          <li>ISBN: <%= @book.isbn %></li>
+          <li>Edition: <%= @book.edition %></li>
+          <li>Publisher: <%= @book.publisher %></li>
+          <li>Publication Date: <%= @book.publication_date %></li>
+        </ul>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Show</button>
+        </div>
+      </div>
     </div>
     """
   end
